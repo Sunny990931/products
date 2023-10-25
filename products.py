18c711eef255ca1c50a9e3424eff18b5d95aaf81
Sunny990931 <sunny990931@gmail.com>
2023-10-25 11:44:03 +0800

    add read file

--- products.py
+++ products.py
@@ -1,4 +1,14 @@
+#讀取檔案
 products= []
+with open('products.csv', 'r') as f :
+	for line in f:
+		if '商品, 價格' in line:
+			continue
+		name, price =line.strip().split(',') #用.split(',')做分隔 = , .strip()除換行符號=(\n)
+		products.append([name, price])
+print(products)
+
+#讓使用者輸入
 while True:
 	name = input('請輸入商品名稱: ')
 	if name == 'q' :
@@ -10,9 +20,11 @@ while True:
 	products.append(p)
 print(products)
 
+#印出所有購買紀錄
 for p in products:
 	print(p[0], '的價格是', p[1])
 
+#寫入檔案
 with open('products.csv', 'w') as f: #encoding = 'utf-8'解決亂碼問題
 	f.write('商品, 價格\n') #加入程式碼寫欄位
 	for p in products:
